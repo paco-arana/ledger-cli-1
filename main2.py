8f820ae3a62f7e3b9ac878ef0f6f6d342d865551
paco-arana <apaco.soc@gmail.com>
2023-04-30 20:02:49 -0700

    Sunday update

--- main2.py
+++ main2.py
@@ -23,33 +23,7 @@ with open("index.ledger", "r") as ind:
 d_frame = pd.DataFrame(all_entries)
 d_frame = d_frame.explode(["account", "credit"])
 
-
-mask = d_frame['account'] == "Bank:Paypal"
-d_frame = d_frame[mask]
-
-
-
-'''
-is_duplicated = d_frame.duplicated(subset=["date", "description"])
-d_frame.loc[is_duplicated, ["date", "description"]] = ""
-'''
-
-
+# Use tabulate to display
 my_table = tabulate(d_frame, headers="keys", tablefmt="grid", showindex="False")
 
-
-
 print(my_table)
-
-"""
-Desired Format:
-
-entry = {
-    "date": "2012/11/16",
-    "description": "Sold some bitcoins",
-    "account_to": ["Asset:Bitcoin Wallet"],
-    "credit_to": ["-3.5 BTC"],
-    "account_from": "Bank:Paypal",
-    "credit_from": "$42.21"
-}
-"""
